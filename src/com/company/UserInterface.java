0e1bc5cbaaf522eb0507ed7ce60f1ceaa6110d94
ThomasSA-Git <thom45w9@stud.kea.dk>
2022-03-16 12:09:10 +0100

    Push .iml filer

--- src/com/company/UserInterface.java
+++ src/com/company/UserInterface.java
@@ -1,4 +1,56 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class UserInterface {
+
+  Scanner sc = new Scanner(System.in);
+
+  public String getUserInput(){
+
+    return sc.nextLine();
+  }
+
+
+  public void presentGame() {
+    System.out.println("""
+        WELCOME TO <placeholder>
+        You can always write 'help' to see a list of commands
+        """);
+  }
+
+  public void getHelpMenu() {
+    System.out.println("""
+        ---------HELP MENU---------
+        go <direction>: Moves your character to the next room in chosen location
+        look: Look around (You might find something interesting)
+        help: Display this menu
+        exit: Close game
+        """);
+  }
+
+  public void blockedDirection(){
+    System.out.println("You can't go this way.");
+  }
+
+  public void currentRoom(){
+    System.out.println("You're currently in room" + currentRoom(););
+  }
+
+  public void lockedDoor() {
+    System.out.println("      ______\n" +
+        "   ,-' ;  ! `-.\n" +
+        "  / :  !  :  . \\\n" +
+        " |_ ;   __:  ;  |\n" +
+        " )| .  :)(.  !  |\n" +
+        " |\"    (##)  _  |\n" +
+        " |  :  ;`'  (_) (\n" +
+        " |  :  :  .     |\n" +
+        " )_ !  ,  ;  ;  |\n" +
+        " || .  .  :  :  |\n" +
+        " |\" .  |  :  .  |\n" +
+        " |____;----.____|");
+    System.out.println("You can't go this direction. The door is locked.");
+  }
+
 }
