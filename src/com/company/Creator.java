ccfb5372bfe44a1c900284515b05351f7c65e1df
DizzyMoon <mikkel.simons1@gmail.com>
2022-03-28 23:27:34 +0200

    Added ranged weapons

--- src/com/company/Creator.java
+++ src/com/company/Creator.java
@@ -4,69 +4,68 @@ import java.util.ArrayList;
 
 public class Creator {
 
-  Room firstRoom;
-  Player player;
+    Room firstRoom;
+    Player player;
 
-  public void createRooms() {
+    public void createRooms() {
 
-    Interactables interactables1 = new Interactables("Door", "You can't open this.", new ArrayList<Item>());
-    Weapon testWeapon = new Weapon("club", 2, 4);
-    Enemy testEnemy = new Enemy("troll", 20, testWeapon);
-    Room room1 = new Room("Room: 1", "The room contains:\n", new ArrayList<Item>(), interactables1, testEnemy);
-    room1.addToInventory(new Item("key"));
-    room1.addToInventory(new Food("banana", 20));
-    room1.addToInventory(new Weapon("Frying Pan", 3, 5
-    ));
-    System.out.println(room1.getMapInventory());
+        Interactables interactables1 = new Interactables("Door", "You can't open this.", new ArrayList<Item>());
+        Weapon testWeapon = new MeleeWeapon("club", 2, 4);
+        Enemy testEnemy = new Enemy("troll", 20, testWeapon);
+        Room room1 = new Room("Room: 1", "The room contains:\n", new ArrayList<Item>(), interactables1, testEnemy);
+        room1.addToInventory(new Item("key"));
+        room1.addToInventory(new Food("banana", 20));
+        room1.addToInventory(new MeleeWeapon("Frying Pan", 3, 5));
+        room1.addToInventory(new RangedWeapon("Bow", 5, 7, 2));
+        System.out.println(room1.getMapInventory());
 
 
+        Room room2 = new Room("Room: 2", "", new ArrayList<Item>(), interactables1, null);
+        Room room3 = new Room("Room: 3", "", new ArrayList<Item>(), interactables1, null);
+        Room room4 = new Room("Room: 4", "", new ArrayList<Item>(), interactables1, null);
+        Room room5 = new Room("Room: 5", "", new ArrayList<Item>(), interactables1, null);
+        Room room6 = new Room("Room: 6", "", new ArrayList<Item>(), interactables1, null);
+        Room room7 = new Room("Room: 7", "", new ArrayList<Item>(), interactables1, null);
+        Room room8 = new Room("Room: 8", "", new ArrayList<Item>(), interactables1, null);
+        Room room9 = new Room("Room: 9", "", new ArrayList<Item>(), interactables1, null);
 
-    Room room2 = new Room("Room: 2", "", new ArrayList<Item>(), interactables1, null);
-    Room room3 = new Room("Room: 3", "", new ArrayList<Item>(), interactables1, null);
-    Room room4 = new Room("Room: 4", "", new ArrayList<Item>(), interactables1, null);
-    Room room5 = new Room("Room: 5", "", new ArrayList<Item>(), interactables1, null);
-    Room room6 = new Room("Room: 6", "", new ArrayList<Item>(), interactables1, null);
-    Room room7 = new Room("Room: 7", "", new ArrayList<Item>(), interactables1, null);
-    Room room8 = new Room("Room: 8", "", new ArrayList<Item>(), interactables1, null);
-    Room room9 = new Room("Room: 9", "", new ArrayList<Item>(), interactables1, null);
+        firstRoom = room1;
 
-    firstRoom = room1;
+        //ROOM 1
+        room1.setEast(room2);
+        room1.setSouth(room4);
 
-    //ROOM 1
-    room1.setEast(room2);
-    room1.setSouth(room4);
+        //ROOM 2
+        room2.setEast(room3);
+        room2.setWest(room1);
 
-    //ROOM 2
-    room2.setEast(room3);
-    room2.setWest(room1);
+        //ROOM 3
+        room3.setWest(room2);
+        room3.setSouth(room6);
 
-    //ROOM 3
-    room3.setWest(room2);
-    room3.setSouth(room6);
+        //ROOM 4
+        room4.setNorth(room1);
+        room4.setSouth(room7);
 
-    //ROOM 4
-    room4.setNorth(room1);
-    room4.setSouth(room7);
+        //ROOM 5
+        room5.setSouth(room8);
+        room5.setDoorLocked();
 
-    //ROOM 5
-    room5.setSouth(room8);
-    room5.setDoorLocked();
+        //ROOM 6
+        room6.setNorth(room3);
+        room6.setSouth(room9);
 
-    //ROOM 6
-    room6.setNorth(room3);
-    room6.setSouth(room9);
+        //ROOM 7
+        room7.setNorth(room4);
+        room7.setEast(room8);
 
-    //ROOM 7
-    room7.setNorth(room4);
-    room7.setEast(room8);
+        //ROOM 8
+        room8.setNorth(room5);
+        room8.setWest(room7);
+        room8.setEast(room9);
 
-    //ROOM 8
-    room8.setNorth(room5);
-    room8.setWest(room7);
-    room8.setEast(room9);
-
-    //ROOM 9
-    room9.setNorth(room6);
-    room9.setWest(room8);
-  }
+        //ROOM 9
+        room9.setNorth(room6);
+        room9.setWest(room8);
+    }
 }
--- src/com/company/GameEngine.java
+++ src/com/company/GameEngine.java
@@ -2,176 +2,197 @@ package com.company;
 
 public class GameEngine {
 
-  public void runGame() {
-    Creator creator = new Creator();
-    UserInterface ui = new UserInterface();
-    creator.createRooms();
-    Player player = new Player(creator.firstRoom);
-    ui.presentGame();
+    public void runGame() {
+        Creator creator = new Creator();
+        UserInterface ui = new UserInterface();
+        creator.createRooms();
+        Player player = new Player(creator.firstRoom);
+        ui.presentGame();
 
-    boolean running = true;
-    while (running) {
+        boolean running = true;
+        while (running) {
 
-      ui.printCurrentRoom(player.getCurrentRoom());
+            ui.printCurrentRoom(player.getCurrentRoom());
 
-      String userInput = ui.getUserInput();
-      switch (userInput) {
-        case "go east" -> {
-          if (player.getCurrentRoom().getEast() == null) {
-            ui.blockedDirection();
-          } else {
-            player.setCurrentRoom(player.getCurrentRoom().getEast());
-          }
-        }
-        case "go north" -> {
-          if (player.getCurrentRoom().getNorth() == null) {
-            ui.blockedDirection();
-          } else {
-            player.setCurrentRoom(player.getCurrentRoom().getNorth());
-          }
-        }
-        case "go west" -> {
-          if (player.getCurrentRoom().getWest() == null) {
-            ui.blockedDirection();
-          } else {
-            player.setCurrentRoom(player.getCurrentRoom().getWest());
-          }
-        }
-        case "go south" -> {
-          if (player.getCurrentRoom().getSouth() == null) {
-            ui.blockedDirection();
-          } else {
-            player.setCurrentRoom(player.getCurrentRoom().getSouth());
-          }
-        }
-        case "inventory" -> ui.printArrayList(player.getPlayerInventory());
+            String userInput = ui.getUserInput();
+            switch (userInput) {
+                case "go east" -> {
+                    if (player.getCurrentRoom().getEast() == null) {
+                        ui.blockedDirection();
+                    } else {
+                        player.setCurrentRoom(player.getCurrentRoom().getEast());
+                    }
+                }
+                case "go north" -> {
+                    if (player.getCurrentRoom().getNorth() == null) {
+                        ui.blockedDirection();
+                    } else {
+                        player.setCurrentRoom(player.getCurrentRoom().getNorth());
+                    }
+                }
+                case "go west" -> {
+                    if (player.getCurrentRoom().getWest() == null) {
+                        ui.blockedDirection();
+                    } else {
+                        player.setCurrentRoom(player.getCurrentRoom().getWest());
+                    }
+                }
+                case "go south" -> {
+                    if (player.getCurrentRoom().getSouth() == null) {
+                        ui.blockedDirection();
+                    } else {
+                        player.setCurrentRoom(player.getCurrentRoom().getSouth());
+                    }
+                }
+                case "inventory" -> ui.printArrayList(player.getPlayerInventory());
 
-        case "take" -> {
-          boolean containsItem = false;
-          ui.printString("What do you want to take?");
-          String take = ui.getUserInput();
-          for (int i = 0; i < player.getCurrentRoom().getMapInventory().size(); i++) {
-            if (player.getCurrentRoom().getMapInventory().get(i).getDescription().equalsIgnoreCase(take)) {
-              player.takeItem(player.getCurrentRoom(), player.getCurrentRoom().getMapInventory().get(i));
-              containsItem = true;
+                case "take" -> {
+                    boolean containsItem = false;
+                    ui.printString("What do you want to take?");
+                    String take = ui.getUserInput();
+                    for (int i = 0; i < player.getCurrentRoom().getMapInventory().size(); i++) {
+                        if (player.getCurrentRoom().getMapInventory().get(i).getDescription().equalsIgnoreCase(take)) {
+                            player.takeItem(player.getCurrentRoom(), player.getCurrentRoom().getMapInventory().get(i));
+                            containsItem = true;
+                        }
+                    }
+                    if (!containsItem) {
+                        ui.printString("This item does not exist");
+                    }
+                }
+
+                case "drop" -> {
+                    ui.printString("What do you want to drop?");
+                    String drop = ui.getUserInput();
+                    for (int i = 0; i < player.getPlayerInventory().size(); i++) {
+                        if (player.getPlayerInventory().get(i).getDescription().equalsIgnoreCase(drop)) {
+                            player.dropItem(player.getCurrentRoom(), player.getPlayerInventory().get(i));
+                        } else {
+                            ui.printString("This item does not exist");
+                        }
+                    }
+                }
+                case "look" -> {
+                    ui.printString(player.getCurrentRoom().getDescription());
+                    ui.printList(player.getCurrentRoom().getMapInventory());
+                }
+                case "help" -> ui.getHelpMenu();
+                case "exit" -> running = false;
+                case "health" -> {
+                    ui.printString("Health: " + player.getHealth());
+                    if (player.getHealth() <= 50) {
+                        try {
+                            ui.printOneLetterAtATime("Narrator: You look alive enough, but i wouldn't wrestle a bear in your condition", 0.05);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+
+                case "equip" -> {
+                    ui.printString("What do you want to equip?");
+                    String weapon = ui.getUserInput();
+                    boolean found = false;
+                    for (int i = 0; i < player.getPlayerInventory().size(); i++) {
+                        if (player.getPlayerInventory().get(i).getDescription().equalsIgnoreCase(weapon)) {
+                            found = true;
+                            Weapon foundWeapon = (Weapon) player.getPlayerInventory().get(i);
+                            player.setEquippedWeapon(foundWeapon);
+                        }
+                    }
+                }
+
+                case "deequip" -> player.deEquip();
+
+                case "weapon" -> {
+                    if (player.getEquippedWeapon() != null) {
+                        if (player.getEquippedWeapon() instanceof RangedWeapon) {
+                            ui.printString(player.getEquippedWeapon() + ", Damage: " + player.getEquippedWeapon().getMinDamage() + " - " + player.getEquippedWeapon().getMaxDamage() + ", Ammo: " + ((RangedWeapon) player.getEquippedWeapon()).getAmmo());
+                        } else if (player.getEquippedWeapon() instanceof MeleeWeapon) {
+                            ui.printString(player.getEquippedWeapon() + ", Damage: " + player.getEquippedWeapon().getMinDamage() + " - " + player.getEquippedWeapon().getMaxDamage());
+                        }
+                    } else {
+                        ui.printString("You have no weapon equipped.");
+                    }
+                }
+                case "attack" -> {
+                    boolean engaged = true;
+                    if (player.getCurrentRoom().getEnemy() != null) {
+                        while (engaged) {
+                            if (player.getHealth() > 0) {
+                                if (player.getEquippedWeapon() instanceof RangedWeapon tmpWeapon) {
+                                    int tmpAmmo = tmpWeapon.getAmmo();
+                                    if (tmpAmmo > 0) {
+                                        tmpAmmo--;
+                                        tmpWeapon.setAmmo(tmpAmmo);
+                                        player.attack();
+                                        ui.printString("You have " + tmpAmmo + " arrows left");
+                                    } else {
+                                        ui.printString("You're out of ammunition!");
+                                        engaged = false;
+                                    }
+                                } else {
+                                    player.attack();
+                                }
+                            } else if (player.getHealth() <= 0) {
+                                engaged = false;
+                                ui.printString("You have been killed. The game is over.");
+                                running = false;
+
+                            }
+                            if (player.getCurrentRoom().getEnemy().getHealth() > 0) {
+                                player.takeDamage(player.getCurrentRoom().getEnemy().attack());
+                                ui.printString("You have " + player.getHealth() + " health left.");
+                            } else if (player.getCurrentRoom().getEnemy().getHealth() <= 0) {
+                                engaged = false;
+                                player.getCurrentRoom().addToInventory(player.getCurrentRoom().getEnemy().dropWeapon());
+                                ui.printString("Your enemy has been defeated.");
+                            }
+                        }
+                    } else if (player.getCurrentRoom().getEnemy() == null) {
+                        ui.printString("Narrater: ");
+                        try {
+                            ui.printOneLetterAtATime("Who are you attacking? There's no one there.", 0.05);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+                case "eat" -> {
+                    ui.printString("What do you want to eat?");
+                    String food = ui.getUserInput();
+                    boolean found = false;
+                    for (int i = 0; i < player.getPlayerInventory().size(); i++) {
+                        if (player.getPlayerInventory().get(i).getDescription().equals(food)) {
+                            found = true;
+                            Food foundFood = (Food) player.getPlayerInventory().get(i);
+                            player.eat(foundFood);
+                            player.getPlayerInventory().remove(foundFood);
+                        }
+                        if (!found) {
+                            ui.printString("You can't eat that!");
+                        }
+                    }
+                }
             }
-          }
-          if (!containsItem) {
-            ui.printString("This item does not exist");
-          }
         }
-
-        case "drop" -> {
-          ui.printString("What do you want to drop?");
-          String drop = ui.getUserInput();
-          for (int i = 0; i < player.getPlayerInventory().size(); i++) {
-            if (player.getPlayerInventory().get(i).getDescription().equalsIgnoreCase(drop)) {
-              player.dropItem(player.getCurrentRoom(), player.getPlayerInventory().get(i));
-            } else {
-              ui.printString("This item does not exist");
-            }
-          }
-        }
-        case "look" -> {
-          ui.printString(player.getCurrentRoom().getDescription());
-          ui.printList(player.getCurrentRoom().getMapInventory());
-        }
-        case "help" -> ui.getHelpMenu();
-        case "exit" -> running = false;
-        case "health" -> {
-          ui.printString("Health: " + player.getHealth());
-          if (player.getHealth() <= 50){
-            try {
-              ui.printOneLetterAtATime("Narrator: You look alive enough, but i wouldn't wrestle a bear in your condition", 0.05);
-            } catch (InterruptedException e) {
-              e.printStackTrace();
-            }
-          }
-        }
-
-        case "equip" -> {
-          ui.printString("What do you want to equip?");
-          String weapon = ui.getUserInput();
-          boolean found = false;
-          for (int i = 0; i < player.getPlayerInventory().size(); i++){
-            if (player.getPlayerInventory().get(i).getDescription().equalsIgnoreCase(weapon)){
-              found = true;
-              Weapon foundWeapon = (Weapon) player.getPlayerInventory().get(i);
-              player.setEquippedWeapon(foundWeapon);
-            }
-          }
-        }
-
-        case "deequip" -> player.deEquip();
-
-        case "weapon" -> {
-          if (player.getEquippedWeapon() != null) {
-            ui.printString(player.getEquippedWeapon() + ", Damage: " + player.getEquippedWeapon().getMinDamage() + " to " + player.getEquippedWeapon().getMaxDamage());
-          } else {
-            ui.printString("You have no weapon equipped.");
-          }
-        }
-        case "attack" -> {
-          boolean engaged = true;
-          if (player.getCurrentRoom().getEnemy() != null) {
-          while(engaged){
-            if(player.getHealth() > 0){
-            player.attack();}
-            else if (player.getHealth() <= 0){
-              engaged = false;
-              ui.printString("You have been killed. The game is over.");
-              //End game?
-
-            }
-            if(player.getCurrentRoom().getEnemy().getHealth() > 0){
-            player.takeDamage(player.getCurrentRoom().getEnemy().attack());
-            ui.printString("You have " + player.getHealth() + " health left.");}
-            else if(player.getCurrentRoom().getEnemy().getHealth() <= 0){
-              engaged = false;
-              player.getCurrentRoom().addToInventory(player.getCurrentRoom().getEnemy().dropWeapon());
-              ui.printString("Your enemy has been defeated.");
-            }
-          }
-          }
-          else if(player.getCurrentRoom().getEnemy() == null){
-            ui.printString("Who are you attacking? There's no one there.");
-          }
-        }
-        case "eat" -> {
-          ui.printString("What do you want to eat?");
-          String food = ui.getUserInput();
-          boolean found = false;
-          for (int i = 0; i < player.getPlayerInventory().size(); i++) {
-            if (player.getPlayerInventory().get(i).getDescription().equals(food)) {
-              found = true;
-              Food foundFood = (Food) player.getPlayerInventory().get(i);
-              player.eat(foundFood);
-              player.getPlayerInventory().remove(foundFood);
-            }
-            if (!found) {
-              ui.printString("You can't eat that!");
-            }
-          }
-        }
-      }
     }
-  }
 
-  public boolean checkTake(String input) {
-    return getPrefix(input).equals("take");
-  }
+    public boolean checkTake(String input) {
+        return getPrefix(input).equals("take");
+    }
 
-  public int getSpace(String input) {
-    return input.indexOf(" ");
-  }
+    public int getSpace(String input) {
+        return input.indexOf(" ");
+    }
 
-  public String getPrefix(String input) {
-    return input.substring(0, getSpace(input));
-  }
+    public String getPrefix(String input) {
+        return input.substring(0, getSpace(input));
+    }
 
-  public String getUserSuffix(String input) {
-    int space = getSpace(input);
-    return input.substring(space + 1);
-  }
+    public String getUserSuffix(String input) {
+        int space = getSpace(input);
+        return input.substring(space + 1);
+    }
 }
 
