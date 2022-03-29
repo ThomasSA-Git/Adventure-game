1399bd6fa5c12eb31a53666390c27d9d1f0a8636
ThomasSA-Git <thom45w9@stud.kea.dk>
2022-03-29 10:09:43 +0200

    NPC class tilføjet

---
+++ src/com/company/NPC.java
@@ -0,0 +1,41 @@
+package com.company;
+
+public class NPC {
+  private String NPCname;
+  private String NPCdescription;
+  private Item item;
+
+  public NPC(String name, String description, Item item){
+    this.NPCname = name;
+    this.NPCdescription = description;
+    this.item = item;
+  }
+
+  public Item getItem() {
+    return item;
+  }
+
+  public void setItem(Item item) {
+    this.item = item;
+  }
+
+  public Item takeItem(){
+    return item;
+  }
+
+  public String getNPCdescription() {
+    return NPCdescription;
+  }
+
+  public String getNPCname() {
+    return NPCname;
+  }
+
+  public void dialogue(){
+
+  }
+
+  public void setHostile(){
+
+  }
+}
