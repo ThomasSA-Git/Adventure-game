3d7bcabd34d660e90c6e1c2621cef9c4c71ffc21
ThomasSA-Git <thom45w9@stud.kea.dk>
2022-03-29 10:33:48 +0200

    NPC class tilføjet

---
+++ src/com/company/Prisoner.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Prisoner extends NPC{
+
+  public Prisoner(String name, String description, Item item){
+    super(name, description, item);
+  }
+
+  public void dialogue(){
+
+  }
+
+  public void turnHostile(){
+
+  }
+
+}
