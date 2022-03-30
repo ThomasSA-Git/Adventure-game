2c643a2cede7644334408401be95a78fdbfb9c62
ThomasSA-Git <thom45w9@stud.kea.dk>
2022-03-30 20:37:23 +0200

    Metoder for switch commands

--- src/com/company/GameEngine.java
+++ src/com/company/GameEngine.java
@@ -45,122 +45,32 @@ public class GameEngine {
           }
         }
 
-        case "open" -> {
-          ui.printString("What do you want to open?");
-          String door = ui.getUserInput();
-          boolean found = false;
-          for (int i = 0; i < player.getCurrentRoom().getDoors().size(); i++) {
-            if (player.getCurrentRoom().getDoors().get(i).getDescription().equalsIgnoreCase(door)) {
-              found = true;
-              Door tmpDoor = player.getCurrentRoom().getDoors().get(i);
-              if (tmpDoor.getLocked()) {
-                ui.printString("Door is locked!");
-              } else {
-                player.setCurrentRoom(tmpDoor.getLeadsTo());
-              }
-            }
-            if (!found) {
-              ui.printString("There is nothing like that to open");
-            }
-          }
-        }
+        case "open" -> open(player, ui);
         case "inventory" -> ui.printArrayList(player.getPlayerInventory());
-
-        case "take" -> {
-          boolean containsItem = false;
-          ui.printString("What do you want to take?");
-          String take = ui.getUserInput();
-          for (int i = 0; i < player.getCurrentRoom().getMapInventory().size(); i++) {
-            if (player.getCurrentRoom().getMapInventory().get(i).getDescription().equalsIgnoreCase(take)) {
-              player.takeItem(player.getCurrentRoom(), player.getCurrentRoom().getMapInventory().get(i));
-              containsItem = true;
-            }
-          }
-          if (!containsItem) {
-            ui.printString("This item does not exist");
-          }
-        }
-
+        case "take" -> take(player, ui);
         case "drop" -> drop(player, ui);
-        case "look" -> {
-          ui.printString(player.getCurrentRoom().getDescription());
-          ui.printString("The following items are in the area:");
-          ui.printList(player.getCurrentRoom().getMapInventory());
-          if (player.getCurrentRoom().getNPC() != null) {
-            ui.printString(player.getCurrentRoom().getNPC().getNpcName());
-          }
-        }
+        case "look" -> look(player, ui);
         case "help" -> ui.getHelpMenu();
         case "exit" -> running = false;
-        case "health" -> {
-          ui.printString("Health: " + player.getHealth());
-          if (player.getHealth() <= 50) {
-            try {
-              ui.printOneLetterAtATime("Narrator: You look alive enough, but i wouldn't wrestle a bear in your condition", 0.05);
-            } catch (InterruptedException e) {
-              e.printStackTrace();
-            }
-          }
-        }
-
+        case "health" -> health(player, ui);
         case "equip" -> equip(player, ui);
-
-        case "use" -> {
-          ui.printString("What do you want to use?");
-          String keyItem = ui.getUserInput();
-          boolean found = false;
-          Door tmpDoor;
-
-          for (int i = 0; i < player.getPlayerInventory().size(); i++) {
-            if (player.getPlayerInventory().get(i).getDescription().equalsIgnoreCase(keyItem)) {
-              found = true;
-            }
-          }
-          if (!found) {
-            ui.printString("You do not have that item!");
-          }
-          found = false;
-          ui.printString("What do you want to use it on?");
-          String keySlot = ui.getUserInput();
-          for (int i = 0; i < player.currentRoom.getDoors().size(); i++) {
-            if (player.currentRoom.getDoors().get(i).getDescription().equalsIgnoreCase(keySlot)) {
-              found = true;
-              tmpDoor = player.currentRoom.getDoors().get(i);
-              tmpDoor.unlockDoor();
-            }
-          }
-          if (!found) {
-            ui.printString("You can't use that item on that!");
-          }
-        }
-
+        case "use" -> use(player, ui);
         case "deequip" -> player.deEquip();
-
-        case "weapon" -> {
-          if (player.getEquippedWeapon() != null) {
-            if (player.getEquippedWeapon() instanceof RangedWeapon) {
-              ui.printString(player.getEquippedWeapon() + ", Damage: " + player.getEquippedWeapon().getMinDamage() + " - " + player.getEquippedWeapon().getMaxDamage() + ", Ammo: " + ((RangedWeapon) player.getEquippedWeapon()).getAmmo());
-            } else if (player.getEquippedWeapon() instanceof MeleeWeapon) {
-              ui.printString(player.getEquippedWeapon() + ", Damage: " + player.getEquippedWeapon().getMinDamage() + " - " + player.getEquippedWeapon().getMaxDamage());
-            }
-          } else {
-            ui.printString("You have no weapon equipped.");
-          }
-        }
-        case "talk" -> {
-          ui.printString("Who or what do you want to talk to?");
-          String talk = ui.getUserInput();
-          if (player.getCurrentRoom().getNPC().getNpcName().equalsIgnoreCase(talk)) {
-            ui.printString(player.getCurrentRoom().getNPC().dialogue());
-          } else {
-            ui.printString("That doesn't talk");
-          }
-        }
+        case "weapon" -> weapon(player, ui);
+        case "talk" -> talk(player, ui);
         case "give" -> give(player, ui);
         case "attack" -> attackSequence(player, ui);
-
         case "eat" -> eat(player, ui);
       }
+    }
+  }
+
+  public void look(Player player, UserInterface ui) {
+    ui.printString(player.getCurrentRoom().getDescription());
+    ui.printString("The following items are in the area:");
+    ui.printList(player.getCurrentRoom().getMapInventory());
+    if (player.getCurrentRoom().getNPC() != null) {
+      ui.printString(player.getCurrentRoom().getNPC().getNpcName());
     }
   }
 
@@ -226,8 +136,27 @@ public class GameEngine {
     }
   }
 
-  public void weapon(Player player, UserInterface ui) {
+  public void health(Player player, UserInterface ui) {
+    ui.printString("Health: " + player.getHealth());
+    if (player.getHealth() <= 50) {
+      try {
+        ui.printOneLetterAtATime("Narrator: You look alive enough, but i wouldn't wrestle a bear in your condition", 0.05);
+      } catch (InterruptedException e) {
+        e.printStackTrace();
+      }
+    }
+  }
 
+  public void weapon(Player player, UserInterface ui) {
+    if (player.getEquippedWeapon() != null) {
+      if (player.getEquippedWeapon() instanceof RangedWeapon) {
+        ui.printString(player.getEquippedWeapon() + ", Damage: " + player.getEquippedWeapon().getMinDamage() + " - " + player.getEquippedWeapon().getMaxDamage() + ", Ammo: " + ((RangedWeapon) player.getEquippedWeapon()).getAmmo());
+      } else if (player.getEquippedWeapon() instanceof MeleeWeapon) {
+        ui.printString(player.getEquippedWeapon() + ", Damage: " + player.getEquippedWeapon().getMinDamage() + " - " + player.getEquippedWeapon().getMaxDamage());
+      }
+    } else {
+      ui.printString("You have no weapon equipped.");
+    }
   }
 
   public void equip(Player player, UserInterface ui) {
@@ -250,8 +179,69 @@ public class GameEngine {
     }
   }
 
-  public void take(Player player, UserInterface ui) {
+  public void open(Player player, UserInterface ui) {
 
+    ui.printString("What do you want to open?");
+    String door = ui.getUserInput();
+    boolean found = false;
+    for (int i = 0; i < player.getCurrentRoom().getDoors().size(); i++) {
+      if (player.getCurrentRoom().getDoors().get(i).getDescription().equalsIgnoreCase(door)) {
+        found = true;
+        Door tmpDoor = player.getCurrentRoom().getDoors().get(i);
+        if (tmpDoor.getLocked()) {
+          ui.printString("Door is locked!");
+        } else {
+          player.setCurrentRoom(tmpDoor.getLeadsTo());
+        }
+      }
+      if (!found) {
+        ui.printString("There is nothing like that to open");
+      }
+    }
+  }
+
+  public void take(Player player, UserInterface ui) {
+    boolean containsItem = false;
+    ui.printString("What do you want to take?");
+    String take = ui.getUserInput();
+    for (int i = 0; i < player.getCurrentRoom().getMapInventory().size(); i++) {
+      if (player.getCurrentRoom().getMapInventory().get(i).getDescription().equalsIgnoreCase(take)) {
+        player.takeItem(player.getCurrentRoom(), player.getCurrentRoom().getMapInventory().get(i));
+        containsItem = true;
+      }
+    }
+    if (!containsItem) {
+      ui.printString("This item does not exist");
+    }
+  }
+
+  public void use(Player player, UserInterface ui) {
+    ui.printString("What do you want to use?");
+    String keyItem = ui.getUserInput();
+    boolean found = false;
+    Door tmpDoor;
+
+    for (int i = 0; i < player.getPlayerInventory().size(); i++) {
+      if (player.getPlayerInventory().get(i).getDescription().equalsIgnoreCase(keyItem)) {
+        found = true;
+      }
+    }
+    if (!found) {
+      ui.printString("You do not have that item!");
+    }
+    found = false;
+    ui.printString("What do you want to use it on?");
+    String keySlot = ui.getUserInput();
+    for (int i = 0; i < player.currentRoom.getDoors().size(); i++) {
+      if (player.currentRoom.getDoors().get(i).getDescription().equalsIgnoreCase(keySlot)) {
+        found = true;
+        tmpDoor = player.currentRoom.getDoors().get(i);
+        tmpDoor.unlockDoor();
+      }
+    }
+    if (!found) {
+      ui.printString("You can't use that item on that!");
+    }
   }
 
   public void drop(Player player, UserInterface ui) {
@@ -288,7 +278,13 @@ public class GameEngine {
   }
 
   public void talk(Player player, UserInterface ui) {
-
+    ui.printString("Who or what do you want to talk to?");
+    String talk = ui.getUserInput();
+    if (player.getCurrentRoom().getNPC().getNpcName().equalsIgnoreCase(talk)) {
+      ui.printString(player.getCurrentRoom().getNPC().dialogue());
+    } else {
+      ui.printString("That doesn't talk");
+    }
   }
 
   public void give(Player player, UserInterface ui) {
