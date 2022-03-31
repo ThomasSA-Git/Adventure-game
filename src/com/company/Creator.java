24eec05de64c1290407f3ffd9a01c2234643d227
ThomasSA-Git <thom45w9@stud.kea.dk>
2022-03-31 14:04:43 +0200

    NPC class udvidet

--- src/com/company/Creator.java
+++ src/com/company/Creator.java
@@ -14,42 +14,42 @@ public class Creator {
     Room room1 = new Room("Cabin", "You find yourself in what looks like a cabin on a larger ship. The room has one door\n" +
         "You feel the shift of gravity going from one end of the room to the other in a rhythmic motion\n" +
         "Atop the sheets of one of the bunk beds you spot a shiny object\n" +
-        "The room contains:\n", new ArrayList<Item>(), new ArrayList<Door>(), null, null);
+        "The room contains:\n", new ArrayList<Item>(), new ArrayList<Door>(), null, null, new ArrayList<Box>());
 
 
     Room room2 = new Room("Galley", "You look around to see that you find yourself in a quaint small kitchen.\n" +
         "This must be the galley of the ship.\n" +
         "On the kitchen isle is a small electrical stove with one pot sitting on top\n" +
         "In the sink lies a dirty frying pan, someone refused to wash (inspired by true events)\n" +
-        "Opposite the door where you entered, there's another white door", new ArrayList<Item>(), new ArrayList<Door>(), null, null);
+        "Opposite the door where you entered, there's another white door", new ArrayList<Item>(), new ArrayList<Door>(), null, null, new ArrayList<Box>());
 
     Room room3 = new Room("Ship Hold", "Barrels, crates, bags and sacks. This is clearly the hold of the ship\n" +
         "To your left there's a small staircase leading up to a hatch.\n" +
         "In front of you, you see an old rusty metal door.\n" +
         "Otherwise, the only thing that catches your eye is one specific crate with a skull on it." +
-        "", new ArrayList<Item>(), new ArrayList<Door>(), null, null);
+        "", new ArrayList<Item>(), new ArrayList<Door>(), null, null, new ArrayList<Box>());
 
     Room room4 = new Room("Brig", "This room smells like booze and something else absolutely ghastly.\n" +
         "to the left you see a wall of metal bars. A jail cell. This must be the brig\n" +
         "Inside sits a ragged man with a crooked smile." +
         "Next to him in the corner of his cell sits a bucket which is obviously the source of the foul smell." +
-        "After a more thorough peak you realise the bucket is full of (you guessed it) anchovies.", new ArrayList<Item>(), new ArrayList<Door>(), prisoner, null);
+        "After a more thorough peak you realise the bucket is full of (you guessed it) anchovies.", new ArrayList<Item>(), new ArrayList<Door>(), prisoner, null, new ArrayList<Box>());
 
     Room room5 = new Room("Deck (Stern)", "Finally fresh air. The ship is surrounded by ocean as far as the eye can see.\n" +
         "In the middle of the deck stands the capstan with a large lever besides it\n" +
-        "You look up and spot something shiny in the ship's mast", new ArrayList<Item>(), new ArrayList<Door>(), null, null);
+        "You look up and spot something shiny in the ship's mast", new ArrayList<Item>(), new ArrayList<Door>(), null, null, new ArrayList<Box>());
 
     Room room6 = new Room("Deck (Bow)", "You stand at the foot of the ships mast.\n" +
-        "A robe ladder leads up to the crow's nest", new ArrayList<Item>(), new ArrayList<Door>(), null, null);
+        "A robe ladder leads up to the crow's nest", new ArrayList<Item>(), new ArrayList<Door>(), null, null, new ArrayList<Box>());
 
     Room room7 = new Room("Mast", "Even with this view, there's no land in sight. There's however, a parrot sitting on the railing.\n" +
-        "The parrot is screetching several controversal political opinions. How highly inapropriate.", new ArrayList<Item>(), new ArrayList<Door>(), null, null);
+        "The parrot is screetching several controversal political opinions. How highly inapropriate.", new ArrayList<Item>(), new ArrayList<Door>(), null, null, new ArrayList<Box>());
 
     Room room8 = new Room("Bridge Deck", "A sense of power washes over you when you stand before the ships helm.\n" +
         "Upon closer inspection, the helm is not attached to anything.\n" +
-        "On the wall is an emergency glass cabinet.", new ArrayList<Item>(), new ArrayList<Door>(), null, null);
+        "On the wall is an emergency glass cabinet.", new ArrayList<Item>(), new ArrayList<Door>(), null, null, new ArrayList<Box>());
 
-    Room room9 = new Room("Captains Quarters", "", new ArrayList<Item>(), new ArrayList<Door>(), null, null);
+    Room room9 = new Room("Captains Quarters", "", new ArrayList<Item>(), new ArrayList<Door>(), null, null, new ArrayList<Box>());
 
     firstRoom = room1;
 
